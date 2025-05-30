c7552bd38089ae8841924f01eb885f966849abeb
Jelle <jellesombekke@gmail.com>
2025-05-30 17:41:57 +0200

    PORT update

--- server.py
+++ server.py
@@ -137,4 +137,5 @@ def run_pipeline(img, timestamp):
     return base64_frames, file_path_list
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
